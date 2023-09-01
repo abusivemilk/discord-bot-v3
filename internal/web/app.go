fbe61c37de4a084b491359f2527f9fbd2c6281fa
Jon <jonhasacat8@gmail.com>
2023-09-01 00:39:11 -0700

    Enable CORS

--- internal/web/app.go
+++ internal/web/app.go
@@ -2,11 +2,16 @@ package web
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
 func App() *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://vatusa.net"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	// API routes
 	e.GET("/assignRoles/:id", assignRoles)
